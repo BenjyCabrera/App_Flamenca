4fd5678d02cee45a20f4e61c512bd7a4c33b6d78
BenjyCabrera <benjamin.cabrera.arroyo89@gmail.com>
2023-09-19 14:04:19 +0200

    primera version

---
+++ script.js
@@ -0,0 +1,32 @@
+const heartWithNames = `
+  ****  *****
+******* *******         Cassandra
+***************
+***************             &
+ *************
+   *********              Benjy
+     *****
+
+      ()_()  
+     ( o.o ) Gotham
+      > ^ <
+
+       ()_() 
+      ( o.o ) Arya
+       > ^ <
+
+ 
+`;
+
+console.log(heartWithNames);
+
+{
+  "type": "inRange",
+  "usaFunciones": false,
+  "filter": "2021-01-14",
+  "diasSumar": 0,
+  "usaFuncionesTo": true,
+  "filterTo": null,
+  "diasSumarTo": 7
+  }
+  
